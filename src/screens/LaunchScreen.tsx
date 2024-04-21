87cc15fa0e0a36c982532b37ac2566aa83b4abe0
Vishnu Chandra MC <vishnuchandra.mc@gmail.com>
2024-04-21 23:01:55 +0530

    Added animation

--- src/screens/LaunchScreen.tsx
+++ src/screens/LaunchScreen.tsx
@@ -6,23 +6,45 @@ import {defaultStyles} from '../constants/Styles';
 import {useNavigation} from '@react-navigation/native';
 import {SCREENS} from '../constants/Strings';
 import {NativeStackNavigationProp} from '@react-navigation/native-stack';
+import Animated, {
+  useSharedValue,
+  withSpring,
+  useAnimatedStyle,
+  Easing,
+  withTiming,
+} from 'react-native-reanimated';
 
 const LaunchScreen = () => {
   const navigation = useNavigation<NativeStackNavigationProp<{route: {}}>>();
   useEffect(() => {
     console.log('Launch Screen');
-    setTimeout(() => {
-      //@ts-ignore
-      navigation.replace(SCREENS.LOGIN);
-    }, 5000);
+    startAnimation();
+    // setTimeout(() => {
+    //   //@ts-ignore
+    //   navigation.replace(SCREENS.LOGIN);
+    // }, 5000);
   }, []);
 
+  const scale = useSharedValue(8); // Initial scale value set to zoom in
+
+  const startAnimation = () => {
+    scale.value = withTiming(1, {
+      duration: 400,
+    });
+  };
+
+  const animatedStyle = useAnimatedStyle(() => {
+    return {
+      transform: [{scale: scale.value}],
+    };
+  });
   return (
     <ScreenContent backgroundColor={Colors.primary} barstyle="light-content">
       <View style={styles.container}>
         <Text style={[defaultStyles.headline, {color: Colors.light}]}>
           tamaCLI
         </Text>
+        <Animated.View style={[styles.stamp, animatedStyle]} />
       </View>
     </ScreenContent>
   );
@@ -35,5 +57,11 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  stamp: {
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+    backgroundColor: 'red',
   },
 });
